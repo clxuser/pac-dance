1dd7653c287c6b3d10d5348d244d30f4fde89819
Player LTUGG <playerltugg@gmail.com>
2025-06-12 15:43:12 +0300

    Add multiplayer score tracking system
    Co-authored-by: clxuser <playerltugg@gmail.com>

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ class Game {
         this.score = 0;
         this.highScore = 0;
         this.isRunning = false;
+        this.playerScores = new Map();
+        this.currentPlayer = null;
     }
 
     start() {
@@ -20,11 +22,31 @@ class Game {
         console.log(`Game over! Final score: ${this.score}`);
     }
 
+    setCurrentPlayer(playerName) {
+        this.currentPlayer = playerName;
+        if (!this.playerScores.has(playerName)) {
+            this.playerScores.set(playerName, 0);
+        }
+    }
+
     addPoints(points) {
         if (this.isRunning) {
             this.score += points;
+            if (this.currentPlayer) {
+                const playerScore = this.playerScores.get(this.currentPlayer) + points;
+                this.playerScores.set(this.currentPlayer, playerScore);
+                console.log(`${this.currentPlayer}'s score: ${playerScore}`);
+            }
             console.log(`Score: ${this.score}`);
         }
+    }
+
+    getPlayerScore(playerName) {
+        return this.playerScores.get(playerName) || 0;
+    }
+
+    getAllPlayerScores() {
+        return Object.fromEntries(this.playerScores);
     }
 
     getHighScore() {
